7ac0f40cee900d9877f3151243de8b44f96b7b69
Sergei Lysogor <me@serhioli.net>
2022-07-27 01:21:04 +0400

    Apply fixes from StyleCI (#6)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/palindromeLinkedList/ListNode.php
+++ src/palindromeLinkedList/ListNode.php
@@ -7,9 +7,9 @@ class ListNode
     public int $val = 0;
     public ?ListNode $next = null;
 
-    public function __construct(int $val = 0, ?ListNode $next = null)
+    public function __construct(int $val = 0, ?self $next = null)
     {
-        $this->val  = $val;
+        $this->val = $val;
         $this->next = $next;
     }
 }
