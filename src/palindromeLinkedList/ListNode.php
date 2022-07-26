93600f9f1c6d7b9887aca68d226f2f0aeaa9ac7f
Sergei Lysogor <me@serhioli.net>
2022-07-27 01:11:22 +0400

    Fix Psalm alerts (#5)

--- src/palindromeLinkedList/ListNode.php
+++ src/palindromeLinkedList/ListNode.php
@@ -5,9 +5,9 @@ namespace serhioli\leetcode\palindromeLinkedList;
 class ListNode
 {
     public int $val = 0;
-    public ListNode|null $next = null;
+    public ?ListNode $next = null;
 
-    public function __construct(int $val = 0, ListNode $next = null)
+    public function __construct(int $val = 0, ?ListNode $next = null)
     {
         $this->val  = $val;
         $this->next = $next;
--- src/twoSum/Solution.php
+++ src/twoSum/Solution.php
@@ -6,13 +6,18 @@ namespace serhioli\leetcode\twoSum;
 
 class Solution
 {
+    /**
+     * @psalm-param list<int> $nums
+     *
+     * @psalm-return array<int, int>|null
+     */
     public function twoSum(array $nums, int $target): ?array
     {
         $remains = array_flip($nums);
 
         foreach ($nums as $index => $value) {
             $remainExpected = $target - $value;
-            $remainIndex = $remains[$remainExpected] ?? null;
+            $remainIndex    = $remains[$remainExpected] ?? null;
             if (!$remainIndex || $remainIndex === $index) {
                 continue;
             }
